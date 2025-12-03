5921cc4fc3a46220f3aaf32ac6736a73913b34ab
A719\dam1 <ricorenzdenis@gmail.com>
2025-12-03 21:17:21 +0100

    termiando ej 3

--- Programacion_modular/src/main/java/org/example/Bateria_1_ej3.java
+++ Programacion_modular/src/main/java/org/example/Bateria_1_ej3.java
@@ -3,22 +3,34 @@ package org.example;
 import java.util.Scanner;
 
 public class Bateria_1_ej3 {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Introduce una cadena...");
         String cadena = teclado.next();
 
         System.out.println(imprimirCadenas(cadena));
-
-
-
-    };
+        System.out.println(contadorvocales(imprimirCadenas(cadena)));
+    }
 
     public static String imprimirCadenas (String cadena){
 
         String cadena_m = cadena.toUpperCase();
 
         return cadena_m;
+    }
+
+    public static int contadorvocales(String cadenas_m){
+
+        int contador = 0;
+
+        for (int i = 0; i < cadenas_m.length(); i++) {
+            if (cadenas_m.charAt(i)=='A' || cadenas_m.charAt(i)=='E' || cadenas_m.charAt(i)=='I' || cadenas_m.charAt(i)=='O' || cadenas_m.charAt(i)=='U'){
+                contador++;
+            }
+
+        }
+
+        return contador;
     }
 }
