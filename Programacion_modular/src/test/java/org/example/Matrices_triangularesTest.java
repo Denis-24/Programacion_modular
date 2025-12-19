a6ec6ab6e262ac71ab33e5cf97c7d716fc4590bd
SPIKI\ricod <denricren@alu.edu.gva.es>
2025-12-19 23:31:22 +0100

    terminado del todo el proyecto

--- Programacion_modular/src/test/java/org/example/Matrices_triangularesTest.java
+++ Programacion_modular/src/test/java/org/example/Matrices_triangularesTest.java
@@ -7,15 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class Matrices_triangularesTest {
 
     @Test
-    void matriz() {
-
-    }
-
-    @Test
     void revision() {
+        int[][] matriz = { {1, 0, 0}, {0, 2, 0}, {0, 0, 3} };
+        boolean resultado = Matrices_triangulares.revision(3, matriz);
+        assertTrue(resultado);
+
+        matriz = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        resultado = Matrices_triangulares.revision(3, matriz);
+        assertFalse(resultado);
     }
 
     @Test
     void respuesta() {
+        assertAll(() -> assertEquals("SI!", Matrices_triangulares.respuesta(true)),
+                () -> assertEquals("NO!", Matrices_triangulares.respuesta(false)));
     }
 }
