6937797eba579de1d47334fd615a04ca1c4fcef8
A719\dam1 <ricorenzdenis@gmail.com>
2026-01-07 21:20:13 +0100

    1

---
+++ Programacion_modular/src/main/java/org/example/recursividad.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class recursividad {
+static int limite=10;
+
+
+    public static void main() {
+        int n=0;
+        do {
+            
+        }
+    }
+
+    public static void imprimir(int num){
+
+        if (num<=limite){
+            System.out.println(num);
+            imprimir(num+1);
+        }
+
+
+    }
+
+
+
+
+}
